ae8b6c2a5fea19fe1cc06d035118e476ed7e236f
Marcos Antônio <marcanantins@gmail.com>
2024-11-15 16:10:17 -0300

    Código de interação feito :D

--- scripts/move.gd
+++ scripts/move.gd
@@ -1,15 +1,30 @@
-extends CharacterBody2D
+extends RigidBody2D
 
-var speed = 500;
+var speed: float = 400;
+var raycast: Object;
+var interagiu: bool = false;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	raycast = get_child(2);
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var diry = (1 if Input.is_action_pressed("ui_down") else 0) - (1 if Input.is_action_pressed("ui_up") else 0);
-	var dirx = (1 if Input.is_action_pressed("ui_right") else 0) - (1 if Input.is_action_pressed("ui_left") else 0);
-	position += Vector2(delta * speed * dirx, delta * speed * diry);
+	var diry = (1 if Input.is_action_pressed("ui_down") || Input.is_key_pressed(KEY_S) else 0) - (1 if Input.is_action_pressed("ui_up") || Input.is_key_pressed(KEY_W) else 0);
+	var dirx = (1 if Input.is_action_pressed("ui_right") || Input.is_key_pressed(KEY_D) else 0) - (1 if Input.is_action_pressed("ui_left") || Input.is_key_pressed(KEY_A) else 0);
+	var dir = Vector2(dirx, diry);
+	linear_velocity = speed * dir;
+	
+	if dir != Vector2.ZERO:
+		raycast.target_position = 100 * dir.normalized();
+	
+	if Input.is_key_pressed(KEY_SPACE):
+		if !interagiu:
+			interagiu = true;
+			var hit = raycast.get_collider();
+			if hit != null && hit.has_method("interact"):
+				hit.interact();
+	else:
+		interagiu = false;
 	
